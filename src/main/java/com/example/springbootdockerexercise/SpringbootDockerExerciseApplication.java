53a714f9b3343573dd2a353d9b74572f9c59de18
yudaeon <daeon03050@gmail.com>
2023-05-16 14:09:37 +0900

    feat: project build

---
+++ src/main/java/com/example/springbootdockerexercise/SpringbootDockerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerExerciseApplication.class, args);
+    }
+
+}
